4100b0a993689ebd1f6ccf18a2d70cede167b913
Abdul Kholid <dul.kholid@gmail.com>
2024-01-03 07:17:58 +0700

    feat(music control button): change play icon color, add hover bg color to prev and next button

--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -5,19 +5,19 @@ const Page1 = () => {
   return (
     <div className="w-full h-screen flex items-center justify-center bg-blue-500">
       {/* music nav button */}
-      <div className="px-10 py-5 rounded-lg flex items-center justify-between bg-white shadow-md w-[300px]">
+      <div className="px-10 py-5 rounded-lg flex items-center justify-between bg-white shadow-md">
         {/* Prev Button */}
-        <button className="w-12 h-12 rounded-full flex items-center justify-center">
+        <button className="w-12 h-12 rounded-full flex items-center justify-center hover:bg-gray-300">
           <span className="triangle prev-icon"></span>
         </button>
 
         {/* Play/Pause Button */}
-        <button className="w-16 h-16 bg-orange-500 rounded-full flex items-center justify-center">
+        <button className="w-16 h-16 mx-5 bg-orange-500 rounded-full flex items-center justify-center">
           <span className="triangle play-icon"></span>
         </button>
 
         {/* Next Button */}
-        <button className="w-12 h-12 rounded-full flex items-center justify-center">
+        <button className="w-12 h-12 rounded-full flex items-center justify-center hover:bg-gray-300">
           <span className="triangle next-icon"></span>
         </button>
       </div>
